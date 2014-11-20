1303bd8a755054554443fc1dfc4cd6175377c90a
mauriciopsgv <mauriciopsgv@gmail.com>
2014-11-20 15:57:50 -0200

    Validate About complete

--- hackapp/app/controllers/about_controller.rb
+++ hackapp/app/controllers/about_controller.rb
@@ -1,10 +1,32 @@
 class AboutController < ApplicationController
   	def index
-
+  		@validate = Hash.new
+  		@validate[:name] = 0
+  		@validate[:email] = 0
+  		@validate[:message] = 0
   	end
 
   	def sendMessage
- 		@message = params[:message_form]
+  		@validate = Hash.new
+  		@message = params[:message_form]
+
+
+ 		if @message[:name].blank?
+ 			@validate[:name] = 1
+  		end
+		
+  		if @message[:email].blank?
+  			@validate[:email] = 1
+  		end
+
+  		if @message[:message].blank?
+  			@validate[:message] = 1
+  		end
+
+  		if @message[:name].blank? || @message[:message].blank? || @message[:email].blank?
+  			render "index" 
+		end
+
  		#AboutNotifier.send_about_email(@message).deliver
 	end
 
