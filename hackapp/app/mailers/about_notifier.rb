8f0bf39888394f4e3b3c8b596fd6f5a7a6c843bd
mauriciopsgv <mauriciopsgv@gmail.com>
2014-11-20 13:11:38 -0200

    About funcionando HELL YEAH

--- hackapp/app/mailers/about_notifier.rb
+++ hackapp/app/mailers/about_notifier.rb
@@ -1,3 +1,10 @@
 class AboutNotifier < ActionMailer::Base
-  default from: "from@example.com"
+  default to: "mauriciopsgv@gmail.com"
+	
+
+	def send_about_email(message)
+		@message = message
+		mail(from: @message[:email], subject: @message[:subject])
+	end
+
 end
