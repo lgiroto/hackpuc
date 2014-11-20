82d26ad1a658f57c870a026733a27f1f23c16597
Ian Albuquerque <ian.albuquerque.silva@gmail.com>
2014-11-20 02:18:27 -0200

    Register and TopBar

--- hackapp/config/routes.rb
+++ hackapp/config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get 'home/quatro'
   post 'home/new'
   get 'home/showAll'
+  get 'users/register'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
