6e1bf40d5df1fba8711eeda0a3ed5c9a2cdf641c
Roberto Bandeira de Mello <beto_bmms@hotmail.com>
2014-11-21 02:55:49 -0200

    aba minhas presc

--- hackapp/config/routes.rb
+++ hackapp/config/routes.rb
@@ -23,6 +23,7 @@ Rails.application.routes.draw do
   delete 'logout'  => 'sessions#destroy'
   get 'users/consultas'
   get 'doctors/consultas'
+  get 'users/prescricoes'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
